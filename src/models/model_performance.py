c5760f6dfc597060048a3f030c39555ae2e618f2
Ngoc <ntran19@jhu.edu>
2019-08-20 08:51:22 -0400

    Updated model performance source code

--- src/models/model_performance.py
+++ src/models/model_performance.py
@@ -14,6 +14,11 @@ from sklearn.metrics import mean_absolute_error
 def mean_abs_error(X_train, X_test, y_train, y_test):
     model = RandomForestRegressor(criterion="mae", random_state=123)
     model.fit(X_train, y_train)
-    preds = model.predict(X_test)
-    return mean_absolute_error(y_test, preds)
+    training_preds = model.predict(X_train)
+    test_preds = model.predict(X_test)
+    mae_training = mean_absolute_error(y_train, training_preds)
+    mae_test = mean_absolute_error(y_test, test_preds)
+    print(f"Mean absolute error (training): {mae_training}")
+    print(f"Mean absolute error (test):     {mae_test}")
+    return mae_test
 
