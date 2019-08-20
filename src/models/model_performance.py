ec0d8e0cac535e8577cce0dd264af76cddb31369
Ngoc <ntran19@jhu.edu>
2019-08-20 10:07:43 -0400

    Updated n_estimators

--- src/models/model_performance.py
+++ src/models/model_performance.py
@@ -12,7 +12,7 @@ from sklearn.metrics import mean_absolute_error
 
 
 def mean_abs_error(X_train, X_test, y_train, y_test):
-    model = RandomForestRegressor(criterion="mae", random_state=123)
+    model = RandomForestRegressor(n_estimators=100, criterion="mae", random_state=123)
     model.fit(X_train, y_train)
     training_preds = model.predict(X_train)
     test_preds = model.predict(X_test)
